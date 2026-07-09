f6992c57a97edf642655443ef4ce0eeb2cf18a13
k48864269-maker <k48864269@gmail.com>
2026-07-09 19:32:06 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 
-TOKEN = os.getenv("MTUyNDQzOTkyNTYxNTg4NjU1Nw.GFvnQk.uVxOAzknuMdPoD7nrRcW8BfoLX7qUv6r_LL1RM")
+TOKEN = os.getenv("MTUyNDQzOTkyNTYxNTg4NjU1Nw.Gnrfev.Gcf7fvLF_MTwDiTzwv-LKobPUVIYLtqWA7p1zk")
 
 intents = discord.Intents.default()
 intents.message_content = True
